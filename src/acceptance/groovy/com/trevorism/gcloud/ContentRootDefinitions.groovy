81fb78046e13cbc72de0a8ba6a27fe111b5dff5a
Trevor Brooks <trevorism@gmail.com>
2023-08-12 19:01:34 -0400

    Add some simple acceptance tests

--- src/acceptance/groovy/com/trevorism/gcloud/ContentRootDefinitions.groovy
+++ src/acceptance/groovy/com/trevorism/gcloud/ContentRootDefinitions.groovy
@@ -1,8 +1,37 @@
 package com.trevorism.gcloud
 
-/**
- * @author tbrooks
- */
+import io.cucumber.groovy.EN
+import io.cucumber.groovy.Hooks
 
-this.metaClass.mixin(io.cucumber.groovy.Hooks)
-this.metaClass.mixin(io.cucumber.groovy.EN)
+this.metaClass.mixin(Hooks)
+this.metaClass.mixin(EN)
+
+def contextRootContent
+def pingContent
+
+Given(~/^the application is alive$/) { ->
+    try{
+        new URL("https://datastore.data.trevorism.com/ping").text
+    }
+    catch (Exception ignored){
+        Thread.sleep(10000)
+        new URL("https://datastore.data.trevorism.com/ping").text
+    }
+}
+
+When(~/^I navigate to "([^"]*)"$/) { String url ->
+    contextRootContent = new URL(url).text
+}
+
+When(~/^I navigate to \/ping on "([^"]*)"$/) { String url ->
+    pingContent = new URL("${url}/ping").text
+}
+
+Then(~/^then a link to the help page is displayed$/) { ->
+    assert contextRootContent
+    assert contextRootContent.contains("/help")
+}
+
+Then(~/^pong is returned, to indicate the service is alive$/) { ->
+    assert pingContent == "pong"
+}
